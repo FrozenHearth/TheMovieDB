3dc73f748d97efaffac8f5876c964cce94e0b0f3
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-28 12:50:33 +0530

    Created SimilarMovies component

---
+++ src/components/movies/movie-details/SimilarMovies.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../../../styles/movies/similarMovies.css';
+import { imageURL } from '../../../utils/ImageURL';
+
+export class SimilarMovies extends Component {
+  openMovieInNewTab = id => {
+    const win = window.open(id, '_blank');
+    win.focus();
+  };
+
+  render() {
+    const { similarMovies } = this.props;
+
+    return (
+      <div className="similar-movies-wrapper">
+        {/* <h2>Similar Movies</h2> */}
+        <h1 className="similar-movies-header">Similar Movies</h1>
+        {similarMovies
+          ? similarMovies.map((similarMovie, index) => (
+              <div key={index} className="similar-movies-poster-wrapper">
+                <img
+                  onClick={() => this.openMovieInNewTab(similarMovie.id)}
+                  className="similar-movies-poster"
+                  src={`${imageURL}${similarMovie.poster_path}`}
+                  alt=""
+                />
+              </div>
+            ))
+          : ''}
+      </div>
+    );
+  }
+}
+
+export default SimilarMovies;
