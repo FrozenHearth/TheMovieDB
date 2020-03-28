62cd1cfd0eabade6c52239369e3db87e520838d8
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-29 01:49:38 +0530

    Added redirection to movie details page when clicking on specific movie in search dropdown

--- src/common/Header/SearchResults.js
+++ src/common/Header/SearchResults.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {
-  Paper,
   List,
   ListItem,
   ListItemText,
   Divider,
   Avatar
 } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import '../../styles/header/searchResults.css';
 import { imageURL } from '../../utils/ImageURL';
 
@@ -15,15 +15,25 @@ const styles = {
   root: {
     width: '100%',
     maxWidth: 360,
-    backgroundColor: '#fff',
-    padding: 0
+    backgroundColor: '#101010',
+    padding: 0,
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#3a3a3a'
+    }
+  },
+
+  divider: {
+    backgroundColor: 'rgba(255, 255, 255, 0.12)'
+  },
+  movieLink: {
+    textDecoration: 'none'
   }
 };
 
 class SearchResults extends Component {
   render() {
     const { classes, searchResults, searchTerm } = this.props;
-    console.log(searchTerm);
 
     return (
       <div
@@ -33,22 +43,24 @@ class SearchResults extends Component {
       >
         {searchResults
           ? searchResults.map((item, index) => (
-              <List
-                key={index}
-                className={classes.root}
-                component="nav"
-                aria-label="mailbox folders"
-              >
-                <ListItem button>
-                  <ListItemText primary={item.title} />
-                  {item.backdrop_path ? (
-                    <Avatar src={`${imageURL}${item.backdrop_path}`} />
-                  ) : (
-                    <Avatar alt="avatar" />
-                  )}
-                </ListItem>
-                <Divider />
-              </List>
+              <Link className={classes.movieLink} to={`/movies/${item.id}`}>
+                <List
+                  key={index}
+                  className={classes.root}
+                  component="nav"
+                  aria-label="mailbox folders"
+                >
+                  <ListItem className={classes.listItemHover} button>
+                    <ListItemText primary={item.title} />
+                    {item.backdrop_path ? (
+                      <Avatar src={`${imageURL}${item.backdrop_path}`} />
+                    ) : (
+                      <Avatar alt="avatar" />
+                    )}
+                  </ListItem>
+                  <Divider className={classes.divider} />
+                </List>
+              </Link>
             ))
           : ''}
       </div>
