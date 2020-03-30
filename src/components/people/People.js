54c7d915f9dff135dc03166498bc438d9dd8cda3
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-30 23:52:56 +0530

    Added person details sidebar

--- src/components/people/People.js
+++ src/components/people/People.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { castURL } from '../../utils/apiURLs';
+import '../../styles/people/peopleDetails.css';
+import PeopleDetailsSidebar from './peopleDetailsSidebar';
 
 class People extends Component {
+  state = {
+    castDetails: {}
+  };
   componentDidMount() {
     axios
       .get(
         `${castURL}${this.props.match.params.id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
       )
-      .then(res => console.log(res.data));
-
-    axios
-      .get(
-        `https://api.themoviedb.org/3/person/${this.props.match.params.id}/images?api_key=${process.env.REACT_APP_API_KEY}`
-      )
-      .then(res => console.log(res.data));
+      .then(res => this.setState({ castDetails: res.data }));
   }
   render() {
+    const { castDetails } = this.state;
+    console.log(castDetails);
     return (
-      <div>
-        <span>Hi I am a person!</span>
+      <div className="person-details-container">
+        {castDetails ? (
+          <PeopleDetailsSidebar castDetails={castDetails} />
+        ) : null}
       </div>
     );
   }
