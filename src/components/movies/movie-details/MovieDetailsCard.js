79e1a6b53f1f6f3d87e63668f8ffbbb2e789b745
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-07 00:39:03 +0530

    Created a separate component for movie cast & reworked on styling

--- src/components/movies/movie-details/MovieDetailsCard.js
+++ src/components/movies/movie-details/MovieDetailsCard.js
@@ -7,19 +7,21 @@ import Button from '@material-ui/core/Button';
 import '../../../styles/movies/movieDetailsCard.css';
 import SimilarMovies from './SimilarMovies';
 import { withStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import MovieCast from './MovieCast';
 
 const styles = {
   movieDetailsContainer: {
     position: 'relative',
+    width: '100%',
     display: 'flex',
     justifyContent: 'flex-start',
     color: 'white',
     margin: '8em 0 0 0',
+    overflow: 'hidden',
     overscrollBehavior: 'none',
     boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
     transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
-    backgroundSize: 'cover',
+    backgroundSize: '100%',
     backgroundPosition: 'left top',
     backgroundRepeat: 'no-repeat'
   },
@@ -55,108 +57,98 @@ const styles = {
 
 const MovieDetailsCard = props => {
   const { details, similarMovies, credits, classes } = props;
-  console.log(details);
 
   return (
-    <div
-      className={classes.movieDetailsContainer}
-      style={
-        details.backdrop_path
-          ? {
-              backgroundImage: `linear-gradient(to right, rgba(31, 31, 31, 0.8), rgba(72, 72, 72, 0.9)), 
+    <>
+      <div
+        className={classes.movieDetailsContainer}
+        style={
+          details.backdrop_path
+            ? {
+                backgroundImage: `linear-gradient(to right, rgba(31, 31, 31, 0.8), rgba(72, 72, 72, 0.9)), 
         url(${fullImage}${details.backdrop_path})`
-            }
-          : { backgroundImage: 'none' }
-      }
-    >
-      <aside className="movie-poster-container">
-        {details.poster_path ? (
-          <img
-            className="movie-poster"
-            src={`${fullImage}${details.poster_path}`}
-            alt=""
+              }
+            : { backgroundImage: 'none' }
+        }
+      >
+        <aside className="movie-poster-container">
+          {details.poster_path ? (
+            <img
+              className="movie-poster"
+              src={`${fullImage}${details.poster_path}`}
+              alt=""
+            />
+          ) : null}
+        </aside>
+        <main className="movie-main-content">
+          <h2 className="movie-title">
+            {details.title}{' '}
+            <span className="movie-release-year">
+              ({moment(details.release_date).format('YYYY')})
+            </span>
+          </h2>
+
+          <div className="subtitles-container">
+            <span className="movie-release-date">
+              {moment(details.release_date).format('DD/MM/YYYY')}
+            </span>
+            <span className="vertical-divider"></span>
+            <span className="movie-runtime">{details.runtime} mins</span>
+
+            <span className="vertical-divider"></span>
+            {details.adult === false ? (
+              <span className="movie-category">PG-13</span>
+            ) : (
+              <span className="movie-category">R</span>
+            )}
+            <span className="vertical-divider"></span>
+            {details.genres
+              ? details.genres.map(genre => (
+                  <span className="movie-genres-list" key={genre.id}>
+                    <span className="movie-genres">{genre.name}</span>
+                  </span>
+                ))
+              : ''}
+          </div>
+          <Rating
+            className={classes.movieRating}
+            name="half-rating-read"
+            readOnly
+            value={details.vote_average ? parseFloat(details.vote_average) : 0}
+            precision={0.5}
+            max={10}
           />
-        ) : null}
-      </aside>
-      <main className="movie-main-content">
-        <h2 className="movie-title">
-          {details.title}{' '}
-          <span className="movie-release-year">
-            ({moment(details.release_date).format('YYYY')})
-          </span>
-        </h2>
+          <div className="movie-tagline">{details.tagline || ''}</div>
+          <div className="movie-overview-wrapper">
+            <h2 className="movie-overview">Overview</h2>
+            <p className="movie-overview-content">{details.overview}</p>
+          </div>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`${details.homepage}`}
+          >
+            <Button className={classes.movieHomepage} variant="contained">
+              Homepage
+            </Button>
+          </a>
+          <a
+            className="movie-title-link"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`https://www.imdb.com/title/${details.imdb_id}`}
+          >
+            <Button className={classes.movieIMDBPage}> IMDb Link</Button>
+          </a>
+        </main>
 
-        <div className="subtitles-container">
-          <span className="movie-release-date">
-            {moment(details.release_date).format('DD/MM/YYYY')}
-          </span>
-          <span className="vertical-divider"></span>
-          <span className="movie-runtime">{details.runtime} mins</span>
-
-          <span className="vertical-divider"></span>
-          {details.adult === false ? (
-            <span className="movie-category">PG-13</span>
-          ) : (
-            <span className="movie-category">R</span>
-          )}
-          <span className="vertical-divider"></span>
-          {details.genres
-            ? details.genres.map(genre => (
-                <span className="movie-genres-list" key={genre.id}>
-                  <span className="movie-genres">{genre.name}</span>
-                </span>
-              ))
-            : ''}
-        </div>
-        <Rating
-          className={classes.movieRating}
-          name="half-rating-read"
-          readOnly
-          value={details.vote_average ? parseFloat(details.vote_average) : 0}
-          precision={0.5}
-          max={10}
-        />
-        {/* <div className="cast-details-wrapper">
-          <span className="cast-details-title">Stars: </span>
-          {credits.cast
-            ? credits.cast.slice(0, 4).map((item, index) => (
-                <span key={index} className="movie-cast">
-                  <Link className={classes.castLink} to={`people/${item.id}`}>
-                    {item.name}
-                  </Link>
-                </span>
-              ))
-            : ''}
-        </div> */}
-        <div className="movie-tagline">{details.tagline || ''}</div>
-        <div className="movie-overview-wrapper">
-          <h2 className="movie-overview">Overview</h2>
-          <p className="movie-overview-content">{details.overview}</p>
-        </div>
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href={`${details.homepage}`}
-        >
-          <Button className={classes.movieHomepage} variant="contained">
-            Homepage
-          </Button>
-        </a>
-        <a
-          className="movie-title-link"
-          rel="noopener noreferrer"
-          target="_blank"
-          href={`https://www.imdb.com/title/${details.imdb_id}`}
-        >
-          <Button className={classes.movieIMDBPage}> IMDb Link</Button>
-        </a>
-      </main>
-
-      {/* 
+        {/* 
         <div className="similar-movies">
           <SimilarMovies similarMovies={similarMovies} />
         </div> */}
-    </div>
+      </div>
+      <MovieCast credits={credits} />
+    </>
   );
 };
 
