02e89ba753aad1497c43d524ba4c39f3dc20de6c
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-12 19:28:37 +0530

    Added a component to scroll to top when route changes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import MovieDetails from './components/movies/movie-details/MovieDetails';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import People from './components/people/People';
+import ScrollIntoView from './components/utils/ScrollIntoView';
 
 const rootElement = document.getElementById('root');
 
 render(
   <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route exact path={`/`} component={App} />
-      <Route exact path={`/movie/:id`} component={MovieDetails} />
-      <Route exact path={`/people/:id`} component={People} />
-    </Switch>
+    <ScrollIntoView>
+      <Switch>
+        <Route exact path={`/`} component={App} />
+        <Route exact path={`/movie/:id`} component={MovieDetails} />
+        <Route exact path={`/people/:id`} component={People} />
+      </Switch>
+    </ScrollIntoView>
   </Router>,
   rootElement
 );
