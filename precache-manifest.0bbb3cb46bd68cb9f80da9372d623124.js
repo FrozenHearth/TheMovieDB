3f50d65923d23e554c6d38688f474d5f4f0d41ea
Vishu <brs@Vishus-MacBook-Air.local>
2020-05-04 01:06:43 +0530

    Updates

---
+++ precache-manifest.0bbb3cb46bd68cb9f80da9372d623124.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845a6f3588f14078214b3bf992f75c9c",
+    "url": "/TheMovieDB/index.html"
+  },
+  {
+    "revision": "36db9cb68d227eb57393",
+    "url": "/TheMovieDB/static/css/main.14ce5922.chunk.css"
+  },
+  {
+    "revision": "8f9a2af2e3f477327e87",
+    "url": "/TheMovieDB/static/js/2.d1fb8c25.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TheMovieDB/static/js/2.d1fb8c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36db9cb68d227eb57393",
+    "url": "/TheMovieDB/static/js/main.97ade1e1.chunk.js"
+  },
+  {
+    "revision": "760504bf7a922c5cc781",
+    "url": "/TheMovieDB/static/js/runtime-main.7d96f2e7.js"
+  },
+  {
+    "revision": "9c0bc9f7133842037f2f9803c3a69a1a",
+    "url": "/TheMovieDB/static/media/hero_banner.9c0bc9f7.png"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/TheMovieDB/static/media/tmdbLogo.56fd61ee.svg"
+  }
+]);
